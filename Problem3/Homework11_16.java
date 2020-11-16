64ad9483dacdc8d4e338758e4fd820acce5bc1cd
Charlesccharris <harriscc@student.swosu.edu>
2020-11-16 08:58:03 -0600

    Made and finished the program

--- Problem3/Homework11_16.java
+++ Problem3/Homework11_16.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.lang.Math;
+import java.util.Scanner;
 public class Homework11_16{
 	public static void main(String[] args){
 		/*(Addition quiz)
@@ -10,6 +13,30 @@ public class Homework11_16{
 		What is 5 + 9? 34; Wrong answer. Try again.
 		What is 5 + 9? 12; You already entered 12; Wrong answer. Try again.
 		What is 5 + 9? 14 You got it!*/
-		System.out.println("Hello");
+		Scanner input = new Scanner(System.in);
+		ArrayList<Integer> pastAnswers = new ArrayList<Integer>();
+
+		runAdditionProblem(input, pastAnswers);
+	}
+
+	public static void runAdditionProblem(Scanner input, ArrayList<Integer> pastAnswers){
+		int numberOne = (int)(Math.random() * 10);
+		int numberTwo = (int)(Math.random() * 10);
+
+		System.out.print("What is " + numberOne + " + " + numberTwo + "? ");
+		int answer = input.nextInt();
+
+		while(numberOne + numberTwo != answer){
+			System.out.println("Wrong answer");
+			if(!pastAnswers.contains(answer)){
+				pastAnswers.add(answer);
+			}
+			else{
+				System.out.println("You already guessed " + answer);
+			}
+			System.out.print("What is " + numberOne + " + " + numberTwo + "? ");
+			answer = input.nextInt();
+		}
+		System.out.println("You got it!");
 	}
 }
