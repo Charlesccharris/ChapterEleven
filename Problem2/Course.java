22e1571dd6a65a52a5823ca8f8c5c81684079163
Charlesccharris <harriscc@student.swosu.edu>
2020-11-16 08:37:32 -0600

    Finalized the program

--- Problem2/Course.java
+++ Problem2/Course.java
@@ -1,26 +1,37 @@
+import java.util.ArrayList;
+
 public class Course{
 	private String courseName;
-	private String[] students = new String[100];
 	private int numberOfStudents;
+	private ArrayList<String> ourStudents = new ArrayList<String>();
 
-	public Course(String courseName){
-		this.courseName = courseName;
+	public Course(String newCourseName){
+		this.courseName = newCourseName;
 	}
 
 	public void addStudents(String student){
-		students[numberOfStudents] = student;
+		ourStudents.add(student);
 		numberOfStudents++;
 	}
-
+/*
 	public String[] getStudents(){
+		String[] students = new String[100];
+		for(int index = 0; index < ourStudents.size(); index++){
+			students[index] = ourStudents.get(index);
+		}
 		return students;
 	}
+*/
 
 	public int getNumberOfStudents(){
 		return numberOfStudents;
 	}
 
 	public String getCourseName(){
-		return courseName();
+		return courseName;
+	}
+
+	public void printStudents(){
+		System.out.println("\n" + ourStudents.toString());
 	}
 }
--- Problem2/Homework11_5.java
+++ Problem2/Homework11_5.java
@@ -7,7 +7,20 @@ public class Homework11_5{
 		You should not change the original contract of the Course class
 		(i.e., the definition of the constructors and methods should not be changed,
 		but the private members may be changed.)*/
+		Course course1 = new Course("Data Structures");
+		Course course2 = new Course("Database Systems");
 
-		System.out.println("Hello");
+		course1.addStudents("Peter Jones");
+		course1.addStudents("Kim Smith");
+		course1.addStudents("Anne Kennedy");
+
+		course2.addStudents("Peter Jones");
+		course2.addStudents("Steve Smith");
+
+		System.out.println("Number of students in course1: " + course1.getNumberOfStudents());
+		course1.printStudents();
+
+		System.out.println();
+		System.out.println("Number of students in course2: " + course2.getNumberOfStudents());
 	}
 }
